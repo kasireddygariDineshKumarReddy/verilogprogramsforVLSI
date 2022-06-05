e13c3525be1056eea0bc07f019fed10b0147f459
kasireddygariDineshKumarReddy <71276225+kasireddygariDineshKumarReddy@users.noreply.github.com>
2022-06-05 18:04:41 +0530

    Update T_flipflop.v

--- T_flipflop.v
+++ T_flipflop.v
@@ -9,12 +9,12 @@ always@(posedge clk,posedge rst)
 begin 
      if(rst==1) begin
 	    q<=1'b0; end
-	  else begin
+     else begin
 	    case(T)
 		  1'b0:begin q<=q;end
 		  1'b1:begin q<=~(q); end
-       endcase
-		end
+            endcase
+	end
 end
 assign qn=~q;
 endmodule
